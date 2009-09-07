d24097cd74b7d7a76d0ac617f797e96e5edcdbf9
Alexander Demin <Alexander Demin@localhost>
2009-09-08 02:50:04 +0400

    Minor coding style corrections.

--- token.h
+++ token.h
@@ -11,19 +11,25 @@ class Token {
   explicit Token(int token) : token_(token), value_("") {}
   Token(int token, const std::string& value) : token_(token), value_(value) {}
   Token(const Token& rhs) { copy(rhs); }
+
   void operator=(const Token& rhs) { copy(rhs); }
+
   virtual bool operator==(const Token& rhs) const {
     return token_ == rhs.token_ && value_ == rhs.value_;
   }
+
   virtual bool operator!=(const Token& rhs) const {
     return !operator==(rhs);
   }
+
   int token() const { return token_; }
   const std::string& value() const { return value_; }
   virtual const char* descr() const = 0;
+
  protected:
   int token_;
   std::string value_;
+
  private:
   void copy(const Token& rhs) {
     token_ = rhs.token_;
